b8b628e83f47c97c3e63280eb06f81b5eb1576ea
Breyner08-Arcos <breynerarcos@gmail.com>
2025-09-05 19:48:27 -0500

    Cambios libro

---
+++ Libro.java
@@ -0,0 +1,11 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int paginas;
+
+    public Libro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+        }
+    }
