85c9c370a83bd203d5e63fecd7bfcc57c7ab5957
Breyner08-Arcos <breynerarcos@gmail.com>
2025-09-05 20:51:19 -0500

    Uso de metodos main

--- Main.java
+++ Main.java
@@ -1,3 +1,35 @@
+import java.util.Scanner;
+ 
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Libro Libro1 = new Libro("Cien Años de Soledad", "Gabriel García Márquez", 5, 2);
+        System.out.println("Libro predefinido:");
+        System.out.println(Libro1.toString());
+
+        Libro miLibro = new Libro();
+
+        System.out.println("Ingrese el título del libro:");
+        String titulo = sc.nextLine();
+ 
+        System.out.println("Ingrese el autor del libro:");
+        String autor = sc.nextLine();
+ 
+        System.out.println("Ingrese el número de ejemplares:");
+        int nEjemplares = sc.nextInt();
+ 
+        System.out.println("Ingrese el número de ejemplares prestados:");
+        int nEjemplaresPrestados = sc.nextInt();
+ 
+        miLibro.setTitulo(titulo);
+        miLibro.setAutor(autor);
+        miLibro.setnEJemplares(nEjemplares);
+        miLibro.setnEJemplaresPrestados(nEjemplaresPrestados);
+
+        System.out.println("Libro ingresado por el usuario:");
+        System.out.println(miLibro.toString());
+
+        sc.close();
+    }
 }
